1501a2074802d38fb9a53a07a15d4c7b83b9e67a
Dwira <dwiradwijanataimade@gmail.com>
2025-07-04 23:28:55 +0800

    "Membuat fungsi VillaController.java yang berfungsi untuk meng-handle villa, membuat fungsi CustomerController.java yang berfungsi untuk meng-handle customer,membuat model Villa.java, membuat model VillaAvailability.java yang merupakan output dari endpoint GET /available atau ketersediaan villa berdasarkan tanggal check-in dan check-out, membuat tipe data BedSize berupa enum"

---
+++ src/villa_booking/model/VillaAvailability.java
@@ -0,0 +1,13 @@
+package villabooking.model;
+
+public class VillaAvailability {
+    public int villaId;
+    public String villaName;
+    public String villaDescription;
+    public String villaAddress;
+    public int roomTypeId;
+    public String roomTypeName;
+    public int roomTypeQuantity;
+    public int roomTypePrice;
+    public int availableRooms;
+}
